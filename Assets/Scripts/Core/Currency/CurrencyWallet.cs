d01814b9710dd30a811c9547e3fa226213b7090e
hoyadong1 <8645465as@gmail.com>
2026-08-04 17:44:49 +0900

    fix: HUD 골드를 실제 잔액과 동기화

--- Assets/Scripts/Core/Currency/CurrencyWallet.cs
+++ Assets/Scripts/Core/Currency/CurrencyWallet.cs
@@ -13,7 +13,6 @@ namespace CleanPlanet.Core.Currency
 
         public event Action<int> GoldChanged;
         public event Action<int> GoldAdded;
-        public event Action<int> GoldSpent;
 
         public int Gold => _gold;
 
@@ -43,7 +42,6 @@ namespace CleanPlanet.Core.Currency
 
             _gold -= amount;
             GoldChanged?.Invoke(_gold);
-            GoldSpent?.Invoke(amount);
             return true;
         }
     }
